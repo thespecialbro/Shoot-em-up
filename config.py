6391b4f3f9151f59bf82c29d078bd216500fb7d7
thespecialbro <thespecialbro@gmail.com>
2016-01-13 13:19:53 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,37 @@
+'''
+Config file for SHMUP.py
+
+The default values are in the comment to the right of the value
+
+IMPORTANT:
+Do NOT delete this file.
+It will break the game.
+'''
+
+# ----- Volume ----- #
+
+# 0.0 to 1.0
+music_volume = 0.0 # 0.4
+sound_volume = 0.5 # 0.5
+
+
+# ----- Video settings ----- #
+
+# Window position
+# Top left corner of game window
+wind_pos_x = 100 # 100
+wind_pos_y = 50 # 50
+
+hat = False # False                   (May make game slow on some computers)
+
+debug = False # False
+
+
+# ----- Misc settings ----- #
+
+# Difficulty level
+# 0.5 to 2.0
+difficulty = 1.0 # 1.0
+
+# Shield recharging
+shield_recharge = True # True
